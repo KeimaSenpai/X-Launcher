b6cd4ed86c0e8d77f437f26a41f061b70f79c12b
KeimaSenpai <hg4052650@gmail.com>
2023-11-06 00:10:01 -0500

    update 0.0.0.2
    Mejoras en la interfaz todavia no listo

--- Launcher/launch.py
+++ Launcher/launch.py
@@ -17,16 +17,16 @@ options = {
 }
 user_windows = os.environ['USERNAME']
 minecraft_directory = f"C://Users//{user_windows}//AppData//Roaming//.xlauncher"
-forfe = minecraft_launcher_lib.forge.find_forge_version('1.16.5')
-print(forfe)
+# forfe = minecraft_launcher_lib.forge.find_forge_version('1.16.5')
+# print(forfe)
 
-minecraft_launcher_lib.install.install_minecraft_version(
-    "1.16.5", minecraft_directory)
-print('Instala la 1.16.5')
+# minecraft_launcher_lib.install.install_minecraft_version(
+#     "1.16.5", minecraft_directory)
+# print('Instala la 1.16.5')
 
-minecraft_launcher_lib.forge.install_forge_version(
-    forfe, minecraft_directory)
-print('Instalado Forge')
+# minecraft_launcher_lib.forge.install_forge_version(
+#     forfe, minecraft_directory)
+# print('Instalado Forge')
 
 
 minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(
--- Launcher/main.py
+++ Launcher/main.py
@@ -1,8 +1,43 @@
 import flet as ft
+from flet import Container, icons, Page, IconButton, Row, Text, colors, alignment, Image, Stack, Column
 
 
 def main(page: ft.Page):
-    page.add(ft.Text(value='Hello, world!'))
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
+    page.title = 'X Launcher'
+    page.window_width = 1000
+    page.window_height = 590
+    page.window_resizable = False
+    page.padding = 0
+
+    bg = Image(src=f'/images/bg1.jpg', fit=ft.ImageFit.CONTAIN)
+    logo = Image(src=f'/images/logo.png', height=70)
+
+    def column_with_horiz_alignment(align: ft.CrossAxisAlignment):
+        return ft.Column(
+            [
+                ft.Container(
+                    ft.Column([logo],
+                              alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                              horizontal_alignment=align,
+                              ),
+                ),
+            ]
+        )
+
+    page.add(Stack([
+        bg,
+        ft.Row(
+            [
+                column_with_horiz_alignment(ft.CrossAxisAlignment.CENTER)
+            ],
+            spacing=30,
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    ]),
+
+    )
 
 
-ft.app(target=main)
+ft.app(target=main, assets_dir="assets")
